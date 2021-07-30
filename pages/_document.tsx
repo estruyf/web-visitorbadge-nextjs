9ce711096e2335f8644cf4a898bb5619973dda80
Elio Struyf <info@estruyf.be>
2021-07-30 10:57:19 +0200

    Added bold font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={`true`} />
-          <link href="https://fonts.googleapis.com/css2?family=Baloo+2&family=Montserrat&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Baloo+2&family=Montserrat:wght@400;700&display=swap" rel="stylesheet" />
         </Head>
         <body className={`font-body`}>
           <Main />
