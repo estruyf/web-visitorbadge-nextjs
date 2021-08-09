c611a3bcc896fa9d27a70ea3b2eeb1ade9038b5d
Elio Struyf <info@estruyf.be>
2021-08-09 09:56:05 +0200

    Add gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      if ((window as any).gtag && (window as any).gtag.pageview) {
+        (window as any).gtag.pageview(url)
+      }
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+  
   return (
     <>
       <Component {...pageProps} />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
+export const GA_TRACKING_ID = "G-HMZMVK7EWS";
+
 class MyDocument extends Document {
   render() {
     return (
@@ -8,6 +10,18 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={`true`} />
           <link href="https://fonts.googleapis.com/css2?family=Baloo+2&family=Montserrat:wght@400;500&display=swap" rel="stylesheet" />
+
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+          <script dangerouslySetInnerHTML={{__html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${GA_TRACKING_ID}', {
+              'page_path': window.location.pathname,
+              'anonymize_ip': true
+            });`}} />
+
         </Head>
         <body className={`font-body`}>
           <Main />
