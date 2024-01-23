c2c69f001b32fc95cf49cf07816fa7764844995d
Elio Struyf <elio@struyfconsulting.be>
2024-01-23 14:44:11 +0100

    Fix links

--- pages/index.tsx
+++ pages/index.tsx
@@ -126,150 +126,152 @@ export default function Home() {
     return '';
   };
 
-  return (
-    <>
-      <Head>
-        <title>Create your visitor badge</title>
-        <meta name="description" content="Create your visitor badge which you can use on your website or GitHub profile." />
-        <meta property="og:description" content="Create your visitor badge which you can use on your website or GitHub profile." />
-        <meta property="twitter:description" content="Create your visitor badge which you can use on your website or GitHub profile." />
+  return <>
+    <Head>
+      <title>Create your visitor badge</title>
+      <meta name="description" content="Create your visitor badge which you can use on your website or GitHub profile." />
+      <meta property="og:description" content="Create your visitor badge which you can use on your website or GitHub profile." />
+      <meta property="twitter:description" content="Create your visitor badge which you can use on your website or GitHub profile." />
 
-        <link rel="preconnect" href={process.env.NEXT_PUBLIC_VISITOR_API} />
-        
-        <meta property="og:type" content="website" />
-        <meta property="twitter:card" content="summary_large_image" />
+      <link rel="preconnect" href={process.env.NEXT_PUBLIC_VISITOR_API} />
+      
+      <meta property="og:type" content="website" />
+      <meta property="twitter:card" content="summary_large_image" />
 
-        <meta property="twitter:image" content={`https://visitorbadge.io/preview.png`} />
-        <meta property="og:image" content={`https://visitorbadge.io/preview.png`} />
+      <meta property="twitter:image" content={`https://visitorbadge.io/preview.png`} />
+      <meta property="og:image" content={`https://visitorbadge.io/preview.png`} />
 
-        <meta property="twitter:url" content="" />
-        <meta property="og:url" content="" />
-      </Head>
+      <meta property="twitter:url" content="" />
+      <meta property="og:url" content="" />
+    </Head>
 
-      <Favicons />
+    <Favicons />
 
-      <Page labelColor={labelColor} 
-            countColor={countColor} 
-            badgeStyle={badgeStyle} 
-            username={debounceUsername}>
-        <div>
-          <div className="pb-4 border-b border-gray-200">
-            <h3 className="text-xl leading-6 font-medium text-gray-900">Create your visitor badge</h3>
-            <p className="mt-2 max-w-4xl text-sm text-gray-500">
-              Fill in the following form to get your visitor badge Markdown and image URL.
-            </p>
-          </div>
-
-          <div className={`my-4 mb-12`}>
-            <div className="grid grid-cols-12 gap-6">
-              <div className="col-span-12">
-                <label htmlFor="username" className="block text-sm font-medium text-gray-700">
-                  URL or Username/Repository
-                </label>
-                <input
-                  type="text"
-                  name="username"
-                  id="username"
-                  autoComplete="username"
-                  value={username}
-                  className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                  placeholder="Example: https://github.com/estruyf/github-visitors-badge or estruyf/github-visitors-badge"
-                  onChange={usernameChange}
-                />
-              </div>
-
-              <div className="col-span-12">
-                <label htmlFor="label" className="block text-sm font-medium text-gray-700">
-                  Label
-                </label>
-                <input
-                  type="text"
-                  name="label"
-                  id="label"
-                  value={label}
-                  autoComplete="label"
-                  className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                  placeholder="Default: VISITORS"
-                  onChange={labelChange}
-                />
-              </div>
-
-              <ColorPicker color={labelColor} title={`Label background`} updateColor={(color) => setLabelColor(color)} />
-
-              <ColorPicker color={countColor} title={`Count background`} updateColor={(color) => setCountColor(color)} />
-
-              <Dropdown items={styles} selected={badgeStyle} title={`Badge style`} triggerUpdate={(style) => setBadgeStyle(style)} />
-              
-              <Dropdown items={types} selected={badgeType} title={`Badge type`} triggerUpdate={(type) => setBadgeType(type)} />
-
-              <Dropdown items={labelStyles} selected={labelStyle} title={`Label style`} triggerUpdate={(style) => setLabelStyle(style)} />
-            </div>
-          </div>
+    <Page labelColor={labelColor} 
+          countColor={countColor} 
+          badgeStyle={badgeStyle} 
+          username={debounceUsername}>
+      <div>
+        <div className="pb-4 border-b border-gray-200">
+          <h3 className="text-xl leading-6 font-medium text-gray-900">Create your visitor badge</h3>
+          <p className="mt-2 max-w-4xl text-sm text-gray-500">
+            Fill in the following form to get your visitor badge Markdown and image URL.
+          </p>
         </div>
 
-        <div className={`mt-16`}>
-          <div className="pb-4 border-b border-gray-200">
-            <h3 className="text-xl leading-6 font-medium text-gray-900">The code for you to use</h3>
-            <p className="mt-2 max-w-4xl text-sm text-gray-500">
-              You can use the generated Markdown code or image URL in any of your projects.
-            </p>
+        <div className={`my-4 mb-12`}>
+          <div className="grid grid-cols-12 gap-6">
+            <div className="col-span-12">
+              <label htmlFor="username" className="block text-sm font-medium text-gray-700">
+                URL or Username/Repository
+              </label>
+              <input
+                type="text"
+                name="username"
+                id="username"
+                autoComplete="username"
+                value={username}
+                className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                placeholder="Example: https://github.com/estruyf/github-visitors-badge or estruyf/github-visitors-badge"
+                onChange={usernameChange}
+              />
+            </div>
+
+            <div className="col-span-12">
+              <label htmlFor="label" className="block text-sm font-medium text-gray-700">
+                Label
+              </label>
+              <input
+                type="text"
+                name="label"
+                id="label"
+                value={label}
+                autoComplete="label"
+                className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-3 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                placeholder="Default: VISITORS"
+                onChange={labelChange}
+              />
+            </div>
+
+            <ColorPicker color={labelColor} title={`Label background`} updateColor={(color) => setLabelColor(color)} />
+
+            <ColorPicker color={countColor} title={`Count background`} updateColor={(color) => setCountColor(color)} />
+
+            <Dropdown items={styles} selected={badgeStyle} title={`Badge style`} triggerUpdate={(style) => setBadgeStyle(style)} />
+            
+            <Dropdown items={types} selected={badgeType} title={`Badge type`} triggerUpdate={(type) => setBadgeType(type)} />
+
+            <Dropdown items={labelStyles} selected={labelStyle} title={`Label style`} triggerUpdate={(style) => setLabelStyle(style)} />
           </div>
+        </div>
+      </div>
 
-          <div className="my-4 grid grid-cols-6 gap-6">
+      <div className={`mt-16`}>
+        <div className="pb-4 border-b border-gray-200">
+          <h3 className="text-xl leading-6 font-medium text-gray-900">The code for you to use</h3>
+          <p className="mt-2 max-w-4xl text-sm text-gray-500">
+            You can use the generated Markdown code or image URL in any of your projects.
+          </p>
+        </div>
 
-            <CopyField title={`Markdown`} value={getMarkdownCode()} />
+        <div className="my-4 grid grid-cols-6 gap-6">
 
-            <CopyField title={`Markdown with status link`} value={getMarkdownCodeWithStatus()} />
+          <CopyField title={`Markdown`} value={getMarkdownCode()} />
 
-            <CopyField title={`HTML`} value={getHtmlLink()} />
+          <CopyField title={`Markdown with status link`} value={getMarkdownCodeWithStatus()} />
 
-            <CopyField title={`Image URL`} value={getLink()} />
+          <CopyField title={`HTML`} value={getHtmlLink()} />
+
+          <CopyField title={`Image URL`} value={getLink()} />
+
+          {
+            getLink() && (
+              <div className="col-span-6">
+                <label htmlFor="imageUrl" className="block text-sm font-medium text-gray-700 mb-2">
+                  Result
+                </label>
+    
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img src={getLink()} alt="Visitor badge" />
+              </div>
+            )
+          }
+        </div>
+      </div>
+
+      <div className={`mt-16`}>
+        <div className="mt-8 pb-4 border-b border-gray-200">
+          <h3 className="text-xl leading-6 font-medium text-gray-900">Follow up on your visitor hits</h3>
+          <p className="mt-2 max-w-4xl text-sm text-gray-500">
+            Want to follow up on your visitor hits? With the following links you can do so.
+          </p>
+        </div>
+
+        <div className="my-4 grid grid-cols-6 gap-6">
+
+          <div className="col-span-12">
+            <label htmlFor="status" className="block text-sm font-medium text-gray-700">
+              Status page
+            </label>
 
             {
-              getLink() && (
-                <div className="col-span-6">
-                  <label htmlFor="imageUrl" className="block text-sm font-medium text-gray-700 mb-2">
-                    Result
-                  </label>
-      
-                  {/* eslint-disable-next-line @next/next/no-img-element */}
-                  <img src={getLink()} alt="Visitor badge" />
-                </div>
+              getStatusLink() ? (
+                (<Link
+                  href={getStatusLink()}
+                  id="status"
+                  className="mt-1 block w-full sm:text-sm text-yellow-700"
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  Click here to go to the status page of:{username}
+
+                </Link>)
+              ) : (
+                <p className="mt-1 block w-full sm:text-sm text-blue-500">No link, please fill in a URL or username/repository path.</p>
               )
             }
           </div>
         </div>
-
-        <div className={`mt-16`}>
-          <div className="mt-8 pb-4 border-b border-gray-200">
-            <h3 className="text-xl leading-6 font-medium text-gray-900">Follow up on your visitor hits</h3>
-            <p className="mt-2 max-w-4xl text-sm text-gray-500">
-              Want to follow up on your visitor hits? With the following links you can do so.
-            </p>
-          </div>
-
-          <div className="my-4 grid grid-cols-6 gap-6">
-
-            <div className="col-span-12">
-              <label htmlFor="status" className="block text-sm font-medium text-gray-700">
-                Status page
-              </label>
-
-              {
-                getStatusLink() ? (
-                  <Link href={getStatusLink()}>
-                    <a id="status" className="mt-1 block w-full sm:text-sm text-yellow-700" target="_blank" rel="noopener noreferrer">
-                      Click here to go to the status page of: {username}
-                    </a>
-                  </Link>
-                ) : (
-                  <p className="mt-1 block w-full sm:text-sm text-blue-500">No link, please fill in a URL or username/repository path.</p>
-                )
-              }
-            </div>
-          </div>
-        </div>
-      </Page>
-    </>
-  )
+      </div>
+    </Page>
+  </>;
 }
