6b2ff3b9e2b4f13cd8fbc54c605287edec529563
Elio Struyf <elio@struyfconsulting.be>
2024-03-26 16:11:07 +0100

    Added support link

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import { API } from '../constants/API';
 
-export interface ICTAProps {}
+export interface ICTAProps { }
 
 export const CTA: React.FunctionComponent<ICTAProps> = (props: React.PropsWithChildren<ICTAProps>) => {
-  const [ total, setTotal ] = React.useState<{totalBadges: number; totalHits: number} | null>(null);
+  const [total, setTotal] = React.useState<{ totalBadges: number; totalHits: number } | null>(null);
 
   React.useEffect(() => {
     const getTotal = async () => {
@@ -18,7 +18,7 @@ export const CTA: React.FunctionComponent<ICTAProps> = (props: React.PropsWithCh
     }
 
     getTotal();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, ['']);
 
   return (
@@ -26,12 +26,12 @@ export const CTA: React.FunctionComponent<ICTAProps> = (props: React.PropsWithCh
       <div className="absolute inset-0">
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
-            className="w-full h-full object-cover"
-            src="https://images.unsplash.com/photo-1597733336794-12d05021d510?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60&&sat=-100"
-            alt="" />
+          className="w-full h-full object-cover"
+          src="https://images.unsplash.com/photo-1597733336794-12d05021d510?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60&&sat=-100"
+          alt="" />
         <div className="absolute inset-0 bg-blue-400 mix-blend-multiply" aria-hidden="true" />
       </div>
-        
+
       <div className="relative max-w-4xl mx-auto text-center py-16 px-4 sm:py-20 sm:px-6 lg:px-8">
         <h1 className="text-5xl font-extrabold text-white font-heading">
           A simple visitor badge for your website
@@ -39,7 +39,7 @@ export const CTA: React.FunctionComponent<ICTAProps> = (props: React.PropsWithCh
         {
           total?.totalBadges && total.totalBadges > 0 && (
             <h2 className="text-3xl mt-4 font-extrabold text-white font-heading">
-              Come and join <span className="text-yellow-500">{total.totalBadges}</span> others using our service. 
+              Come and join <span className="text-yellow-500">{total.totalBadges}</span> others using our service.
             </h2>
           )
         }
@@ -52,9 +52,13 @@ export const CTA: React.FunctionComponent<ICTAProps> = (props: React.PropsWithCh
           Want to show how many visitors your site/GitHub profiles gets? Quickly fill in the form on the page, and copy the Markdown or HTML code to the location you want to use it.
         </p>
 
-        <p className="mt-8 text-center">
+        <p className="mt-8 flex space-x-8 justify-center items-center">
           {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img className={`mx-auto h-8 sm:h-10`} src={`${process.env.NEXT_PUBLIC_VISITOR_API}${API.daily}?user=estruyf&repo=github-visitors-badge&labelColor=%23555555&countColor=%23F0B354&label=Visitors today&style=default`} alt={`Daily visitors`} />
+          <img className={`h-8 sm:h-10`} src={`${process.env.NEXT_PUBLIC_VISITOR_API}${API.daily}?user=estruyf&repo=github-visitors-badge&labelColor=%23555555&countColor=%23F0B354&label=Visitors today&style=default`} alt={`Daily visitors`} />
+
+          <a href={`https://github.com/sponsors/estruyf`} title={`Support this project`}>
+            <img className={`h-8 sm:h-10`} src={`https://img.shields.io/badge/support_this_project-555555?style=flat-square&logo=githubsponsors&logoColor=%23FFFFFF&labelColor=%23EA4AAA`} alt={`Support this project`} />
+          </a>
         </p>
       </div>
     </div>
